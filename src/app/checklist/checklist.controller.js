05cfb7a553ac85e98e587bd2a74d5199bf4d6a41
Michal Witkowski <michal.witkowski@pragmatists.pl>
2016-02-10 10:16:35 +0100

    feature is done, no test included

--- src/app/checklist/checklist.controller.js
+++ src/app/checklist/checklist.controller.js
@@ -3,16 +3,17 @@ angular.module('tdd')
 
         var vm = this;
         vm.list = [
-            {name : 'Clean the dishesh', isDone : false},
-            {name : 'Go to work.', isDone : false},
-            {name : 'Do things.', isDone : true}
+            {name : 'Clean the dishesh', isDone : false, type: 'work'},
+            {name : 'Go to work.', isDone : false, type: 'private'},
+            {name : 'Do things.', isDone : true, type: 'home'}
         ];
+        vm.types = ['work', 'home', 'private', ''];
 
         vm.addTask = addTask;
         vm.clearDone = clearDone;
 
-        function addTask(name) {
-            vm.list.push({name : name, isDone : false});
+        function addTask(name, type) {
+            vm.list.push({name : name, isDone : false, type: type});
         }
 
         function clearDone() {
