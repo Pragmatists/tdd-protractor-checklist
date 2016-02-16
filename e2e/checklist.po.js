1d642c9e79c2255931eb2c42f30c6a6e9e4413b1
Pawel Barszcz <pawelbarszcz@gmail.com>
2016-02-16 21:33:46 +0100

    Checklist: test for adding a new task

--- e2e/checklist.po.js
+++ e2e/checklist.po.js
@@ -1,12 +1,28 @@
 module.exports = function checklistPage() {
+  var page = {};
 
-  return {
-    open: open,
-    pageTitle: $('h1')
-  };
+  page.open = open;
+  page.pageTitle = pageTitle;
+  page.addTask = addTask;
+  page.tasks = tasks;
+
+  return page;
 
   function open() {
     browser.get('/#/checklist')
+  }
+
+  function pageTitle() {
+    return $('h1');
+  }
+
+  function addTask(taskName) {
+    $('#name').sendKeys(taskName);
+    $('button[type=submit]').click();
+  }
+
+  function tasks() {
+    return $('.checklist').$('ul').$$('li');
   }
 
 }();
--- e2e/checklist.spec.js
+++ e2e/checklist.spec.js
@@ -1,4 +1,3 @@
-//- Poprawne dodawanie zadań.
 //- Zaznaczanie zadania jako wykonane.
 //- Dodanie odpowiedniej klasy dla tekstu - kiedy zadanie jest wykonane.
 //- Poprawne czyszczenie listy wykonanych zadań.
@@ -8,14 +7,27 @@ describe('Checklist', function () {
   var matchers
     , checklist;
 
+  var NUMBER_OF_TASKS_AT_INIT = 3;
+
   beforeEach(function () {
     matchers = require('./matchers.js');
     checklist = require('./checklist.po.js');
     checklist.open();
   });
 
-  it('should include Aloha', function () {
-    expect(checklist.pageTitle.getText()).toBe('Checklist');
+  it('has title', function () {
+    expect(checklist.pageTitle().getText()).toBe('Checklist');
+  });
+
+  it('has some tasks at init', function () {
+    expect(checklist.tasks().count()).toBe(NUMBER_OF_TASKS_AT_INIT);
+  });
+
+  it('adds a task', function () {
+    checklist.addTask('tiny task');
+
+    expect(checklist.tasks().count()).toBe(NUMBER_OF_TASKS_AT_INIT + 1);
+    expect(checklist.tasks().get(3).getText()).toBe('tiny task');
   });
 
 });
